3f166c8bdc2142af51caa0585d72758b9366187e
CheonUng <cjsdnd9712@gmail.com>
2025-04-18 18:02:31 +0900

    회원가입 테스트 라우트 코드 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/sign-in/SignIn";
 import SignUp from "./pages/sign-up/SignUp"; // 회원가입 페이지 추가
+import KakaoRedirectPage from './pages/kakao-signUp-redirect';//테스트
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<SignIn />} /> {/* 기본 페이지 */}
         <Route path="/signup" element={<SignUp />} /> {/* 회원가입 페이지 */}
+        {/* ✅ 카카오 리다이렉트용 임시 라우트 */}
+        <Route path="/kakao-redirect" element={<KakaoRedirectPage />} />
       </Routes>
     </Router>
   );
