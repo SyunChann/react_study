8956e3d804192ac08bf4572ee522348367be6f3e
SyunChann <chltmdcks3@gmail.com>
2025-05-21 17:19:13 +0900

    오타수정정

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
     return(
         <main>
             <a href="/signin">로그인</a>
-            <a href="/signun">회원가입</a>
+            <a href="/signup">회원가입</a>
         </main>
         
     );
